82320ed2c78fc235f23c5ec6805902627fb0f18a
Gildecy de Jesus Sá <101933897+gildecy@users.noreply.github.com>
2022-12-11 17:25:18 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import Component from'./componente';
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <div>
+    <Component />
+   
+
+   
+    </div>
+  )
+}
+
+  export default App
+
+
---
+++ componente.jsx
@@ -0,0 +1,18 @@
+export function Component() {
+    return (
+        <div>
+
+            <h1
+                style={{
+                    color: 'green',
+                }}> acessando componente estilizado verde </h1>
+            <h2
+            style={{
+                color:'blue',
+            }}> segundo componente estilizado em azul </h2>
+
+        </div>
+    )
+}
+
+export default Component
